a3502152bcd6048fe4fdb1df9cda4f581875f937
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 05:39:04 -0700

    Q 09,10 TS/JS FOR 45 assignment

---
+++ qes9.js
@@ -0,0 +1,6 @@
+/*
+Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that
+reveals your favorite number.Print that meaasge.
+*/
+var favoriteNumber = 7;
+console.log("MY favourite number is ".concat(favoriteNumber, "."));
---
+++ qes9.ts
@@ -0,0 +1,17 @@
+/* 
+Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that 
+reveals your favorite number.Print that meaasge.
+*/ 
+// Question: 10;
+// Adding Comments: Choose two of the programs you've written, and add at least one comment to each.
+// if you don't have anything specific to write because your programs are too simple to this point, just add 
+// your name the current date at the top of each program file.Then write one sentence describing what the program does.
+
+author: [Rakhshi khan]
+date: [ Monday , March 18 , 2024]
+
+//task 9: printing my favourite number.
+
+let favoriteNumber: number = 7;
+// reveling my favourite number in a message formate.
+console.log(`MY favourite number is ${favoriteNumber}.`)
